2adcede203a818d140e7860018eb287cae2b1ade
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-20 12:02:15 -0700

    Create CaretButton.tsx

---
+++ data-viz-platform/src/components/CaretButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type CaretButtonProps = {
+  onClick?: () => void;
+  className?: string;
+};
+
+export default function CaretButton() {
+  return (
+    <div>CaretButton</div>
+  )
+};

395207ab7198d9825640dc131b43c7e2ebd4c4b3
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-20 01:19:53 -0700

    Update App.tsx
    Set up the layout shell in App.tsx with a flex main tag and scrollable content area. I also left a spot for the sidebar.

--- data-viz-platform/src/App.tsx
+++ data-viz-platform/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+type AppProps = {
+  children: React.ReactNode;
+};
+function App({children}: AppProps) {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex h-screen bg-[#0f0f0f] text-white">
+      {/* add sidebarNav */}
+      <div className="flex-1 overflow-y-auto">
+        {children}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 

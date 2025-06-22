5e31c915dc3ac9ba607e68d7876f97bd39c2c47f
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 15:52:06 -0700

    Update EditVariablesButton.tsx

--- data-viz-platform/src/components/EditVariablesButton.tsx
+++ data-viz-platform/src/components/EditVariablesButton.tsx
@@ -1,6 +1,6 @@
 type EditVariablesButtonProps = {
- onClick?: () => void;
- className?: string;
+  onClick?: () => void;
+  className?: string;
 }
 export default function EditVariablesButton({onClick, className = "font-roobert border bg-[#242424] border-[#5a5a5a] rounded-md shadow-[1px_1px_0_rbg(0,0,0)] shadow-black p-2 text-[12px]"}: EditVariablesButtonProps) {
   return (

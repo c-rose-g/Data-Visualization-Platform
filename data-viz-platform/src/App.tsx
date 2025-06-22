95e54a3ff59d75e54a29b953be33712e591556b2
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 01:04:43 -0700

    Update App.tsx

--- data-viz-platform/src/App.tsx
+++ data-viz-platform/src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
+import Sidebar from './components/layout.tsx/Sidebar';
+import TopNav from './components/layout.tsx/TopNav';
+
 type AppProps = {
   children: React.ReactNode;
 };
 function App({children}: AppProps) {
 
   return (
-    <main className="flex h-screen">
-      {/* add sidebarNav */}
-      <div className="flex-1">
-        {children}
+    <div className="flex">
+      <Sidebar/>
+      <div className="flex-1 flex flex-col">
+        <TopNav/>
+        <main className="flex-1 overflow-auto">{children}</main>
       </div>
-    </main>
+    </div>
   )
 }
 

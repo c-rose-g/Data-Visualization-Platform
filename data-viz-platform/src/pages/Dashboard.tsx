fc44d77be40421362c80dcac21ee00195e436b33
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 15:52:35 -0700

    Update Dashboard.tsx

--- data-viz-platform/src/pages/Dashboard.tsx
+++ data-viz-platform/src/pages/Dashboard.tsx
@@ -22,10 +22,15 @@ function Dashboard() {
     setShowMessages(((prev:boolean) => !prev))
   }
 
+  const handlePanelSlideOver = () => {
+    setShowPanel(((prev:boolean) =>!prev))
+    EditVariablesPanel()
+  }
+
   return (
     <>
     {/*Dashboard top header*/}
-    <div className="bg-[#161618] text-white pr-8 pl-8 pt-10 pb-10 h-full">
+    <div className="bg-[#161618] text-white pr-8 pl-8 pt-10 pb-10 h-screen">
       <div id="charging-station-wrapper" className="flex justify-between pb-8">
         <div className="flex items-center">
           <div className="ml-1">{<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6"><path fillRule="evenodd" d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z" clipRule="evenodd" /></svg>}</div>
@@ -33,7 +38,7 @@ function Dashboard() {
         </div>
         <div id="charging-station-btns" className="flex gap-3">
           <RefreshButton onClick={handleRefresh}/>
-          <EditVariablesButton onClick={() => setShowPanel(showing => !showing)}/>
+          <EditVariablesButton onClick={handlePanelSlideOver}/>
           <UploadButton onClick={handleUpload}/>
           </div>
       </div>
@@ -78,7 +83,7 @@ function Dashboard() {
               </div>
             </div>
             {/* Graph content section */}
-            <div id="card-wrapper" className="flex h-[502px] gap-4 justify-between">
+            <div id="card-wrapper" className="flex gap-4 justify-between">
             <div className="w-[660px] bg-[#222324] rounded-md flex border border-[#424242] shadow-[5px_3px_5px_1px_rgb(10,10,12,.35)]">
               <div className="">Graph Card Content</div>
             </div>

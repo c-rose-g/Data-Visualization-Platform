d91381277fdefb72d1be8fd65d539a65231649fb
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 01:05:12 -0700

    Update CaretButton.tsx

--- data-viz-platform/src/components/CaretButton.tsx
+++ data-viz-platform/src/components/CaretButton.tsx
@@ -5,8 +5,8 @@ type CaretButtonProps = {
 
 export default function CaretButton({onClick, className = "" }: CaretButtonProps) {
   return (
-    <button type="button" onClick={onClick } className={`p-1 hover:bg-gray-700 rounded ${className}`} aria-label="Toggle details">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <button type="button" onClick={onClick } className={`pl-2 pr-2 pt-1 pb-1 border border-[#C8E972] text-[#C8E972] ml-80  hover:bg-gray-700 rounded-full ${className}`} aria-label="Toggle details">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
   <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
 </svg>
 

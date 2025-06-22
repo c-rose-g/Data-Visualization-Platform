38e87a8cda908af368e655a1af4468e823a5ce2d
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-21 22:43:30 -0700

    Update tailwind.config.js

--- data-viz-platform/tailwind.config.js
+++ data-viz-platform/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ['"Satoshi"', 'sans-serif'],  
+        roobert: ['"RoobertTrial"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }

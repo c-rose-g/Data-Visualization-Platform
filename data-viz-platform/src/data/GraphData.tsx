3a9a68c2f331480706091275a8c9fbad08a47b93
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-23 12:17:16 -0700

    Update GraphData.tsx

--- data-viz-platform/src/data/GraphData.tsx
+++ data-viz-platform/src/data/GraphData.tsx
@@ -1,8 +1,9 @@
-export type DataPoint = {
+export interface DataPoint {
  month: string;
  value: number;
-}
-export const graphData: DataPoint[] =[
+};
+
+export const graphData: DataPoint[] = [
  { month: 'Apr', value: 40 },
  { month: 'May', value: 50 },
  { month: 'Jun', value: 45 },
@@ -10,4 +11,4 @@ export const graphData: DataPoint[] =[
  { month: 'Aug', value: 60 },
  { month: 'Sep', value: 30 },
  { month: 'Oct', value: 58 },
-]
+];

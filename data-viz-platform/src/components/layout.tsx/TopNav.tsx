38233a390f029f493f7e2b0bc510cbd9d2ee01a0
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 22:43:09 -0700

    Update TopNav.tsx

--- data-viz-platform/src/components/layout.tsx/TopNav.tsx
+++ data-viz-platform/src/components/layout.tsx/TopNav.tsx
@@ -1,12 +1,26 @@
+import HamburgerButton from "../HamburgerButton"
 
-export default function TopNav() {
+type HamburgerButtonProp = {
+  sidebarOpen: boolean;
+  onHamburgerClick?: () => void;
+}
+
+export default function TopNav({sidebarOpen, onHamburgerClick }:HamburgerButtonProp) {
   return (
-    <div className="h-20 bg-[#0d0d0d] text-white pt-[20px] pb-[20px] pl-[24px] pr-[20px]">
-      <div id="top-nav-btns" className="flex gap-6 items-center ">
-        <button type="button" className="bg-[#222324] font-inter text-lg font-medium border border-[#5A5A5A] pt-2 pb-2 pr-6 pl-6 rounded-md">Charging Station</button>
-        <div className="text-lg font-roobert font-medium">Fleet Sizing</div>
-        <div className="font-inter font-medium text-lg">Parking</div>
-      </div>
-    </div>
+    <header className="flex items-center h-20 bg-[#0d0d0d] text-white pt-[20px] pb-[20px] pl-[30px] -ml-[80px]">
+      <HamburgerButton
+      onClick={onHamburgerClick}
+      aria-expanded={sidebarOpen}
+      className="text-white "
+      />
+      <nav id="top-nav-btns" className="flex gap-6 items-center pl-[50px] w-full ">
+        <a href="/charging" className="bg-[#222324] font-inter text-lg font-medium border border-[#5A5A5A] pt-2 pb-2 pr-6 pl-6 rounded-md">Charging Station</a>
+        <a href="/fleet" className="text-lg font-roobert font-medium">Fleet Sizing</a>
+        <a href="/parking" className="font-inter font-medium text-lg">Parking</a>
+        <div className="flex ml-auto pr-[40px]">
+        <input type="search" placeholder="Search" className="font-inter bg-[#181819] text-[rgba(237, 237, 237, 1)] rounded-md px-4 py-2 border border-[rgba(90, 90, 90, 1)]" />
+        </div>
+      </nav>
+    </header>
   )
 }

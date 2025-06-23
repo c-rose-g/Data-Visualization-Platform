eba1ec98709fae5873e0053559555133077380a9
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 23:16:22 -0700

    Update GraphData.ts

--- data-viz-platform/src/data/GraphData.ts
+++ data-viz-platform/src/data/GraphData.ts
@@ -1,1 +1,13 @@
-// dummy data
+export type DataPoint = {
+ month: string
+ value: number
+}
+export const graphData: DataPoint[] =[
+ { month: 'Apr', value: 40 },
+ { month: 'May', value: 50 },
+ { month: 'Jun', value: 45 },
+ { month: 'Jul', value: 88 },
+ { month: 'Aug', value: 60 },
+ { month: 'Sep', value: 30 },
+ { month: 'Oct', value: 58 },
+]

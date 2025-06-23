c529eb7c1c2349e2455ad7bfef6353696407b606
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 22:49:44 -0700

    Update Layout.tsx

--- data-viz-platform/src/components/layout.tsx/Layout.tsx
+++ data-viz-platform/src/components/layout.tsx/Layout.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
+import type { ReactNode } from "react"
 import Sidebar from "./Sidebar"
 import TopNav from "./TopNav"
 
-export default function Layout() {
+export type LayoutProps = {
+  children: ReactNode
+}
+
+export default function Layout({children}:LayoutProps) {
   const [open, setOpen] = useState(true)
 
   return(
@@ -16,6 +21,8 @@ export default function Layout() {
         sidebarOpen={open}
         onHamburgerClick={() =>setOpen(open => !open)}
         />
+        {/* main content */}
+        <main className="flex-1 overflow-auto">{children}</main>
       </div>
       </div>
   )

1d3bef32f90a6541c0037bd2cc512ceee51e68b4
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-23 13:02:29 -0700

    Update firebase.ts

--- data-viz-platform/src/firebase.ts
+++ data-viz-platform/src/firebase.ts
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDT_3fNb7PvbfZGBrhOicY0CRbpQCOaVJE",
-  authDomain: "answersai-demo-54ede.firebaseapp.com",
-  projectId: "answersai-demo-54ede",
-  storageBucket: "answersai-demo-54ede.firebasestorage.app",
-  messagingSenderId: "746471072892",
-  appId: "1:746471072892:web:8b5fb0991f9a358a5d95f9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

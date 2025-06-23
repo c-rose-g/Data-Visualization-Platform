4906213021b086257888404d166ac1c940c39fe7
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-22 22:43:38 -0700

    Update Sidebar.tsx

--- data-viz-platform/src/components/layout.tsx/Sidebar.tsx
+++ data-viz-platform/src/components/layout.tsx/Sidebar.tsx
@@ -1,4 +1,4 @@
-import Hamburger from "../HamburgerButton"
+import { useNavigate } from "react-router-dom"
 import HomeButton from "../HomeButton"
 import BellButton from "../BellButton"
 import ClipboardButton from "../ClipboardButton"
@@ -6,12 +6,19 @@ import CloudUploadButton from "../CloudUploadButton"
 import CogButton from "../CogButton"
 import ProfileButton from "../ProfileButton"
 
-export default function Sidebar() {
+export type SidebarProps = {
+  collapsed?: boolean;
+}
+export default function Sidebar({collapsed}:SidebarProps) {
+  const navigate = useNavigate();
+  const handleHome = () =>{
+    navigate("/dashboard");
+  };
+
   return (
-    <div className="bg-[#0d0d0d] text-white w-20 pt-[30px] pb-[20px] pl-[24px] pr-[20px] flex justify-between flex-col">
+    <div className="mt-[80px] bg-[#0d0d0d] text-white w-20 pt-[30px] pb-[20px] pl-[24px] pr-[20px] flex justify-between flex-col">
       <div id="sidebar-top-btns" className="flex items-center flex-col gap-8">
-        <Hamburger/>
-        <HomeButton/>
+        <HomeButton onClick={handleHome}/>
         <BellButton/>
         <ClipboardButton/>
         <CloudUploadButton/>

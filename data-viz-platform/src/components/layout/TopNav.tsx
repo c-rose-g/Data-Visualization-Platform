82850a9e0ae4db4a5454402063872f39d38d079e
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-23 12:17:27 -0700

    Update TopNav.tsx

--- data-viz-platform/src/components/layout/TopNav.tsx
+++ data-viz-platform/src/components/layout/TopNav.tsx
@@ -7,14 +7,14 @@ type HamburgerButtonProp = {
 
 export default function TopNav({sidebarOpen, onHamburgerClick }:HamburgerButtonProp) {
   return (
-    <header className="flex items-center h-20 bg-[#0d0d0d] text-white pt-[20px] pb-[20px] pl-[30px] -ml-[80px]">
+    <header className="flex items-center h-20 bg-[#0d0d0d] text-white  pl-[30px] -ml-[80px]">
       <HamburgerButton
       onClick={onHamburgerClick}
       aria-expanded={sidebarOpen}
       className="text-white "
       />
-      <nav id="top-nav-btns" className="flex gap-6 items-center pl-[50px] w-full ">
-        <a href="/charging" className="bg-[#222324] font-inter text-lg font-medium border border-[#5A5A5A] pt-2 pb-2 pr-6 pl-6 rounded-md">Charging Station</a>
+      <nav id="top-nav-btns" className="flex gap-6 items-center pl-[50px]">
+        <a href="/charging" className="bg-[#222324] font-inter text-lg font-medium border border-[#5A5A5A] pr-6 pl-6 rounded-md">Charging Station</a>
         <a href="/fleet" className="text-lg font-roobert font-medium">Fleet Sizing</a>
         <a href="/parking" className="font-inter font-medium text-lg">Parking</a>
         <div className="flex ml-auto pr-[40px]">
